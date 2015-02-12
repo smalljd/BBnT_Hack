97b1d851909ce4c2890995942e9fada91617206b
Brian Briggman <brian.briggman@capitalone.com>
2015-02-12 18:32:26 -0500

    Updates for login and maps

--- nodejs/app.js
+++ nodejs/app.js
@@ -28,7 +28,11 @@ var url = "http://fuelhackathon.herokuapp.com/users"
 var express = require('express');
 var fs = require('fs');
 var app = express();
-
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+                              extended: true
+                              }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -40,6 +44,60 @@ app.get('/', function(req, res){
         res.render('index.ejs',{title:"BB and T"});
         });
 
+app.post('/login', function(req, res){
+        request({
+                url: url,
+                json: true
+                },
+                function (error, response, users) {
+                if (!error && response.statusCode === 200) {
+                    console.log("got users");
+                    var currentUserId = 0;
+                    var currentUser;
+                    console.log("username = " + req.body.username);
+                    console.log("password = " + req.body.password);
+                    for(var i=0; i<users.length; i++)
+                    {
+                        console.log("users[i].lastName = " + users[i].lastName);
+                        if(users[i].firstName === req.body.username && users[i].lastName == req.body.password)
+                        {
+                            console.log("MATCH!");
+                            currentUser = users[i];
+                            currentUserId = users[i]._id;
+                            break;
+                        }
+                    }
+                    console.log("currentUser = " + currentUser);
+                    if(currentUser != null) // Successful login!
+                    {
+                        var accountsURL = "http://fuelhackathon.herokuapp.com/accounts/" + currentUserId;
+                        console.log("accountsURL = " + accountsURL);
+                        request({
+                                url: accountsURL,
+                                json: true
+                                },
+                                function (error, response, accounts) {
+                                if (!error && response.statusCode === 200) {
+                                
+                                console.log(accounts) // Print the json response
+                                res.render('presentView.ejs',{title:"Accounts",accounts:accounts});
+//                                fs.readFile('presentView.html', function(err, page) {
+//                                            res.writeHead(200, {'Content-Type': 'text/html'});
+//                                            res.write(page);
+//                                            res.end();
+//                                            });
+                                }
+                                else
+                                res.render('error.ejs');
+                                
+                                })
+                    }
+                    else {
+                        res.render('index.ejs');
+                    }
+                }
+        })
+        });
 
 app.get('/users', function(req, res){
         request({
@@ -109,6 +167,25 @@ app.get('/account/:id', function(req, res, next){
                 
                 console.log(transactions) // Print the json response
                 res.render('account.ejs',{title:"Transactions",transactions:transactions});
+                }
+                else
+                res.render('error.ejs');
+                
+                })
+        });
+
+app.get('/accountMap/:id', function(req, res, next){
+        var transactionsURL = "http://fuelhackathon.herokuapp.com/transactions/" + req.params.id;
+        console.log("transactionsURL = " + transactionsURL);
+        request({
+                url: transactionsURL,
+                json: true
+                },
+                function (error, response, transactions) {
+                if (!error && response.statusCode === 200) {
+                
+                console.log(transactions) // Print the json response
+                res.render('map.ejs',{title:"Transactions",transactions:transactions});
                 }
                 else
                 res.render('error.ejs');
