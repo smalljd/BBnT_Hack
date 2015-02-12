3e4c37424135afdb2850842eb9bbe0ee703faf84
Brian Briggman <brian.briggman@capitalone.com>
2015-02-12 11:57:47 -0500

    Initial node app

---
+++ nodejs/app.js
@@ -0,0 +1,72 @@
+//// Retrieve
+//var MongoClient = require('mongodb').MongoClient;
+//
+//// Connect to the db
+//MongoClient.connect("mongodb://candidate.20.mongolayer.com:10655/app33842458", function(err, db) {
+//                    if(!err) {
+//                    console.log("We are connected");
+//                    }
+//                    });
+
+
+var request = require("request")
+
+var url = "http://fuelhackathon.herokuapp.com/users"
+
+// Sample request
+//request({
+//            url: url,
+//            json: true
+//        },
+//        function (error, response, body) {
+//            if (!error && response.statusCode === 200) {
+//                console.log(body) // Print the json response
+//            }
+//        })
+
+
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res){
+        res.render('index.ejs');
+        });
+
+
+app.get('/users', function(req, res){
+        request({
+                url: url,
+                json: true
+                },
+                function (error, response, users) {
+                if (!error && response.statusCode === 200) {
+                
+                    console.log(users) // Print the json response
+                res.render('index.ejs',{title:"BB and T",users:users});
+
+                }
+                else
+                    res.render('error.ejs');
+
+                })
+        });
+
+app.get('/user/:id', function(req, res, next){
+        res.locals.posts.forEach(function(post){
+                                 if (req.params.slug === post.slug){
+                                 res.render('post.ejs', { post: post });
+                                 }
+                                 })
+        });
+
+app.listen(3000);
+console.log('app is listening at localhost:3000');
+
